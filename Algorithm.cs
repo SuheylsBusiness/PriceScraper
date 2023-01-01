3aea34128da22673ac0cad2b4d571003271302cd
SuheylsBusiness <49858621+SuheylsBusiness@users.noreply.github.com>
2023-01-01 14:45:37 +0100

    Add files via upload

---
+++ Algorithm.cs
@@ -0,0 +1,222 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace PriceScraper
+{
+    internal class Algorithm
+    {
+        static string _environmentPath = "";
+        static string _sheetId = "";
+        static SheetsService _gService = null;
+        static void Main(string[] args)
+        {
+            //Init environment
+            _environmentPath = Path.GetFullPath(Path.Combine(@$"{Environment.CurrentDirectory}", @"..\..\.."));
+            _gService = ImportantMethods.InitializeGoogleSheet();
+            _sheetId = "1S0kkoXtqjR8hylly7rKnOKR9CXBacHt-umtex31CzKA";
+            var allPhoneJsonSellMyMobile = GetPhoneJsonSellMyMobile();
+
+            //algorithm
+            while (true)
+            {
+                try
+                {
+                    var sheetObjs = GetSheetObjs();
+                    for (int i = 0; i < sheetObjs.Count; i++)
+                    {
+                        try
+                        {
+                            if (sheetObjs[i].ProductName.Length < 1 || sheetObjs[i].ProductName.Contains(" ") == false) continue;
+
+                            if (sheetObjs[i].SheetName == "Sell Pricing to scrape")
+                            {
+                                foreach (var phone in allPhoneJsonSellMyMobile)
+                                {
+                                    string name = "", url = "";
+
+                                    try { name = ImportantMethods.FormatStringProperly(phone["name"]); } catch (Exception) { }
+                                    try { url = "https://www.sellmymobile.com" + ImportantMethods.FormatStringProperly(phone["resultsUrl"]); } catch (Exception) { }
+
+                                    if (name == sheetObjs[i].ProductName)
+                                    {
+                                        var urlResponseContent = GetResponse(linkToVisit: url, headerType: "sellmymobile", isPostRequest: false, body: "", useProxy: false);
+                                        var document = ImportantMethods.GetHtmlNode(urlResponseContent);
+                                        var scriptContent = document.QuerySelectorAll("script[type=\"text/javascript\"]").Where(x => x.InnerHtml.Contains("window.serverViewModel")).First().InnerHtml.Replace("window.serverViewModel = ", "").Replace("};", "}");
+                                        var dealsJson = JsonConvert.DeserializeObject<dynamic>(scriptContent)["deals"];
+                                        var allQuotes = new List<QuoteObj>();
+                                        foreach (var deal in dealsJson)
+                                        {
+                                            string condition = "", quote = "";
+
+                                            try { condition = ImportantMethods.FormatStringProperly(deal["condition"]); } catch (Exception) { }
+                                            try { quote = ImportantMethods.FormatStringProperly(deal["quote"]); } catch (Exception) { }
+
+                                            allQuotes.Add(new QuoteObj()
+                                            {
+                                                Condition = condition,
+                                                Quote = quote
+                                            });
+                                        }
+
+                                        sheetObjs[i].NewQuote = allQuotes.Where(x => x.Condition == "New").OrderByDescending(x => double.Parse(x.Quote)).First().Quote;
+                                        sheetObjs[i].WorkingQuote = allQuotes.Where(x => x.Condition == "Working").OrderByDescending(x => double.Parse(x.Quote)).First().Quote;
+                                        sheetObjs[i].BrokenQuote = allQuotes.Where(x => x.Condition == "Broken").OrderByDescending(x => double.Parse(x.Quote)).First().Quote;
+                                        break;
+                                    }
+                                }
+                            }
+                            else if (sheetObjs[i].SheetName == "Ecom Pricing to scrape")
+                            {
+                                var responseContent = GetResponse(linkToVisit: $@"https://www.backmarket.com/en-us/search?q=", headerType: "backmarket", isPostRequest: false, body: "", useProxy: false);
+                                var document = ImportantMethods.GetHtmlNode(responseContent);
+                                var allItems = document.QuerySelectorAll("div[class=\"flex flex-row h-full items-center md:flex-col md:items-start\"]");
+                                foreach (var item in allItems)
+                                {
+                                    string option = "", price = "", name = "";
+
+                                    try { option = Regex.Match(item.QuerySelector("span[class=\"body-2-light duration-200 line-clamp-1 normal-case overflow-ellipsis overflow-hidden text-black transition-all\"]").InnerText, @".+?(?= - )").Value; } catch (Exception) { }
+                                    try { price = ImportantMethods.FormatStringProperly(item.QuerySelector("span[class=\"body-2-bold text-black\"]").InnerText); } catch (Exception) { }
+                                    try { name = ImportantMethods.FormatStringProperly(item.QuerySelector("h2[class=\"body-1-bold duration-200 line-clamp-1 md:mb-1 md:mt-0 mt-1 normal-case overflow-ellipsis overflow-hidden text-black transition-all\"]").InnerText).Split("\n").First(); } catch (Exception) { }
+
+                                    if (option == sheetObjs[i].Option)
+                                    {
+                                        sheetObjs[i].Price = price;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    }
+                    for (int i = 0, r = 1; i < sheetObjs.Count; i++, r++)
+                    {
+                        try
+                        {
+                            if (sheetObjs[i].ProductName.Length < 1 || sheetObjs[i].ProductName.Contains(" ") == false) continue;
+
+                            if (sheetObjs[i].SheetName == "Sell Pricing to scrape")
+                            {
+                                ImportantMethods.UpdateSpreadsheet($"{sheetObjs[i].SheetName}!D{r}:D{r}", new List<object>() { sheetObjs[i].NewQuote }, _gService, _sheetId);
+                                ImportantMethods.UpdateSpreadsheet($"{sheetObjs[i].SheetName}!E{r}:E{r}", new List<object>() { sheetObjs[i].WorkingQuote }, _gService, _sheetId);
+                                ImportantMethods.UpdateSpreadsheet($"{sheetObjs[i].SheetName}!G{r}:G{r}", new List<object>() { sheetObjs[i].BrokenQuote }, _gService, _sheetId);
+                            }
+                            else if (sheetObjs[i].SheetName == "Ecom Pricing to scrape")
+                            {
+                                ImportantMethods.UpdateSpreadsheet($"{sheetObjs[i].SheetName}!G{r}:G{r}", new List<object>() { sheetObjs[i].Price }, _gService, _sheetId);
+                            }
+                            Thread.Sleep(TimeSpan.FromSeconds(2));
+                        }
+                        catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    }
+                    Thread.Sleep(TimeSpan.FromMinutes(0.1));
+                }
+                catch (Exception ex) { }
+            }
+        }
+        private static dynamic GetPhoneJsonSellMyMobile()
+        {
+            return JsonConvert.DeserializeObject<dynamic>(GetResponse(linkToVisit: $@"https://www.sellmymobile.com/ajax/search/phones/", headerType: "sellmymobile", isPostRequest: false, body: "", useProxy: false));
+        }
+        public static string GetResponse(string linkToVisit, string headerType = "", bool isPostRequest = false, string body = "", bool useProxy = false)
+        {
+            var responseContent = "";
+            for (int i = 0; i < 50 && responseContent.Length == 0 || responseContent.Contains("HTTP Error 400"); i++)
+            {
+                var client = new RestClient(linkToVisit)
+                {
+                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+                    //Proxy = new WebProxy()
+                    //{
+                    //    Address = new Uri("http://global.rotating.proxyrack.net:9000"),
+                    //    Credentials = new NetworkCredential("myrate", "fd1544-6fd31d-562dd8-cab3de-e7b041")
+                    //}
+
+                };
+                if (useProxy)
+                {
+                    client.Proxy = new WebProxy()
+                    {
+                        Address = new Uri($"http://user-suheyl:test123@gate.smartproxy.com:7000"),
+                        Credentials = new NetworkCredential("suheyl", "test123")
+                    };
+                }
+                var request = GetAppropriateHeader(headerType, isPostRequest, body);
+
+                var response = client.ExecuteAsync(request);
+
+                if (!response.Wait(300000)) { continue; };
+                responseContent = response.Result.Content;
+                if (responseContent.Length == 0 || responseContent.Contains("HTTP Error 400")) { continue; }
+            }
+
+            return responseContent;
+        }
+        [DebuggerStepThrough]
+        private static RestRequest GetAppropriateHeader(string headerType, bool isPostRequest, string body)
+        {
+            var request = new RestRequest();
+            IEnumerable<FileInfo> headerFile = null;
+
+            if (isPostRequest) request = new RestRequest(Method.POST);
+
+            headerFile = new DirectoryInfo($@"{_environmentPath}\Misc\ApiCookies").GetFiles().Where(x => x.Name == $"{headerType}.txt");
+            var fileContent = File.ReadAllLines(headerFile.First().FullName);
+            foreach (var line in fileContent)
+            {
+                try
+                {
+                    var headerName = Regex.Match(line, @".+?(?=\:)").Value.Trim();
+                    var headerValue = Regex.Match(line, @"(?<=:).*").Value.Trim();
+
+                    //if (headerName.ToLower().Contains("accept-encoding")) { headerValue = "application/json"; }
+
+                    if (headerName.ToLower().Contains("user-agent")) { }
+                    if (headerName.ToLower().Contains("content-length")) { }
+                    else if (headerName.ToLower().Contains("host")) { }
+                    else if (headerName.Contains("{") || headerValue.Contains("{")) { }
+                    else request.AddHeader(headerName, headerValue);
+                }
+                catch (Exception) { }
+            }
+            if (body.Length > 0)
+            {
+                if (headerType == "services2")
+                    request.AddParameter("application/x-www-form-urlencoded; charset=UTF-8", body, ParameterType.RequestBody);
+                else
+                    request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundarywAjxGVrKcQAsXeO6", body, ParameterType.RequestBody);
+            }
+
+            return request;
+        }
+        private static List<SheetObj> GetSheetObjs()
+        {
+            var sheetObjs = new List<SheetObj>();
+            var sheet1Data = ImportantMethods.ReadSpreadsheetEntries("Sell Pricing to scrape!C1:C9999", _gService, _sheetId);
+            //var sheet2Data = ImportantMethods.ReadSpreadsheetEntries("Ecom Pricing to scrape!B1:C9999", _gService, _sheetId);
+            sheetObjs.AddRange(sheet1Data.Select(x => new SheetObj() { SheetName = "Sell Pricing to scrape", ProductName = x.Count() > 0 ? x[0].ToString() : "" }));
+            //sheetObjs.AddRange(sheet2Data.Select(x => new SheetObj() { SheetName = "Ecom Pricing to scrape", ProductName = x.Count() > 0 ? x[0].ToString() : "", Option = x.Count() > 0 ? x[1].ToString() : "" }));
+            return sheetObjs;
+        }
+    }
+    class QuoteObj
+    {
+        public string Quote { get; set; }
+        public string Condition { get; set; }
+    }
+    class SheetObj
+    {
+        public string ProductName { get; set; }
+        public string SheetName { get; set; }
+        public string NewQuote { get; set; }
+        public string WorkingQuote { get; set; }
+        public string BrokenQuote { get; set; }
+        public string Option { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ ImportantMethods.cs
@@ -0,0 +1,203 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using Path = System.IO.Path;
+using System.Diagnostics;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace PriceScraper
+{
+    [DebuggerStepThrough]
+    public class ImportantMethods
+    {
+        public static void UpdateSpreadsheet(string range, List<object> objects, SheetsService sheetService, string googleSheet_sheetID)
+        {
+            var valueRange = new ValueRange();
+            valueRange.Values = new List<IList<object>> { objects };
+            var appendRequest = sheetService.Spreadsheets.Values.Update(valueRange, googleSheet_sheetID, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+            var appendResponse = appendRequest.Execute();
+        }
+        public static HtmlNode GetHtmlNode(string responseStr)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(responseStr);
+            var document = htmlDoc.DocumentNode;
+            return document;
+        }
+        public static string FormatStringProperly(object inputStr)
+        {
+            return HttpUtility.HtmlDecode(inputStr.ToString()).Trim();
+        }
+        public static void ClearSheet(string range, SheetsService sheetService, string googleSheet_sheetID)
+        {
+            // TODO: Assign values to desired properties of `requestBody`:
+            Google.Apis.Sheets.v4.Data.ClearValuesRequest requestBody = new Google.Apis.Sheets.v4.Data.ClearValuesRequest();
+
+            SpreadsheetsResource.ValuesResource.ClearRequest request = sheetService.Spreadsheets.Values.Clear(requestBody, googleSheet_sheetID, range);
+
+            // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+            Google.Apis.Sheets.v4.Data.ClearValuesResponse response = request.Execute();
+
+            // TODO: Change code below to process the `response` object:
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+        }
+        public static void SendEmail(string subject, string body, string destinationEmail, List<string> attachements)
+        {
+            string username = "suheylupworkbot@gmail.com", password = "stumynehzzollpzo";
+            using (System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587))
+            {
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(username, password);
+                MailMessage msgObj = new MailMessage();
+                msgObj.To.Add(destinationEmail);
+                msgObj.From = new System.Net.Mail.MailAddress(username);
+                msgObj.Subject = subject;
+                msgObj.Body = body;
+                msgObj.IsBodyHtml = true;
+                for (int i = 0; i < attachements.Count; i++)
+                    msgObj.Attachments.Add(new Attachment(attachements[i]));
+                client.Send(msgObj);
+            }
+        }
+        public static void SendEmail(string subject, string body, string destinationEmail)
+        {
+            string username = "suheylupworkbot@gmail.com", password = "stumynehzzollpzo";
+            using (System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587))
+            {
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(username, password);
+                MailMessage msgObj = new MailMessage();
+                msgObj.To.Add(destinationEmail);
+                msgObj.From = new System.Net.Mail.MailAddress(username);
+                msgObj.Subject = subject;
+                msgObj.Body = body;
+                msgObj.IsBodyHtml = true;
+                client.Send(msgObj);
+            }
+        }
+        public static void AppentIntoTopSpreadsheet2(List<string> objects, SheetsService sheetService, string googleSheet_sheetID, int sheetId)
+        {
+            InsertDimensionRequest insertRow = new InsertDimensionRequest();
+            insertRow.Range = new DimensionRange()
+            {
+                SheetId = sheetId,
+                Dimension = "ROWS",
+                StartIndex = 1,
+                EndIndex = 2
+            };
+
+            PasteDataRequest data = new PasteDataRequest
+            {
+                Data = string.Join(";%_", objects),
+                Delimiter = ";%_",
+                Coordinate = new GridCoordinate
+                {
+                    ColumnIndex = 0,
+                    RowIndex = 1,
+                    SheetId = sheetId
+                },
+            };
+
+            BatchUpdateSpreadsheetRequest r = new BatchUpdateSpreadsheetRequest()
+            {
+                Requests = new List<Request>
+                {
+                    new Request{ InsertDimension = insertRow },
+                    new Request{ PasteData = data }
+                }
+            };
+
+            BatchUpdateSpreadsheetResponse response1 = sheetService.Spreadsheets.BatchUpdate(r, googleSheet_sheetID).Execute();
+
+        }
+        public static void AppentIntoSpreadsheet(string range, List<object> objects, SheetsService sheetService, string googleSheet_sheetID)
+        {
+            var valueRange = new ValueRange();
+            valueRange.Values = new List<IList<object>> { objects };
+            var appendRequest = sheetService.Spreadsheets.Values.Append(valueRange, googleSheet_sheetID, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            var appendResponse = appendRequest.Execute();
+        }
+        public static void AppentIntoTopSpreadsheet(List<string> objects, SheetsService sheetService, string googleSheet_sheetID, int sheetId)
+        {
+            InsertDimensionRequest insertRow = new InsertDimensionRequest();
+            insertRow.Range = new DimensionRange()
+            {
+                SheetId = sheetId,
+                Dimension = "ROWS",
+                StartIndex = 1,
+                EndIndex = 2
+            };
+
+            PasteDataRequest data = new PasteDataRequest
+            {
+                Data = string.Join(";%_", objects),
+                Delimiter = ";%_",
+                Coordinate = new GridCoordinate
+                {
+                    ColumnIndex = 0,
+                    RowIndex = 1,
+                    SheetId = sheetId
+                },
+            };
+
+            BatchUpdateSpreadsheetRequest r = new BatchUpdateSpreadsheetRequest()
+            {
+                Requests = new List<Request>
+                {
+                    new Request{ InsertDimension = insertRow },
+                    new Request{ PasteData = data }
+                }
+            };
+
+            BatchUpdateSpreadsheetResponse response1 = sheetService.Spreadsheets.BatchUpdate(r, googleSheet_sheetID).Execute();
+
+        }
+        public static IList<IList<object>> ReadSpreadsheetEntries(string range, SheetsService sheetService, string googleSheet_sheetID)
+        {
+            var request = sheetService.Spreadsheets.Values.Get(googleSheet_sheetID, range);
+            var response = request.Execute();
+            var values = response.Values;
+            return values;
+        }
+        public static SheetsService InitializeGoogleSheet()
+        {
+            GoogleCredential credential;
+            string[] googleSheet_scopes = { SheetsService.Scope.Spreadsheets };
+            using (var stream = new FileStream("projectaffordany-1587302173157-d20f1c21561a.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(googleSheet_scopes);
+            }
+
+            SheetsService sheetService;
+            sheetService = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer() { HttpClientInitializer = credential, ApplicationName = "Task", });
+            return sheetService;
+        }
+        public static List<string> removeDuplicatesFromList(List<string> input)
+        {
+            try { input = input.GroupBy(x => x).Select(x => x.First()).ToList(); } catch (Exception) { }
+            return input;
+        }
+    }
+}
